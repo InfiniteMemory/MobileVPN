e06c8de02ad16b6ab5675832b561cdb72587618e
haoliu1205 <haoliu1205@gmail.com>
2014-10-13 19:18:58 +0800

    Initial Commit

---
+++ MobileVPN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileVPN
+//
+//  Created by Jack Liu on 14-10-13.
+//  Copyright (c) 2014年 Jack Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
